265db97ffb25a4e28b09f7bc28163cf847042101
sujeongKim <cme10575@gmail.com>
2021-04-16 01:17:53 +0900

    텝바 아이콘 추가

--- App.tsx
+++ App.tsx
@@ -11,15 +11,38 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+//import { MaterialCommunityIcons } from '@expo/vector-icons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { HomeScreen, MyPageScreen } from './src/pages'
 
 const Tab = createBottomTabNavigator()
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="MyPage" component={MyPageScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#81C4A7',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="MyPage"
+        component={MyPageScreen}
+        options={{
+          tabBarLabel: 'My Page',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
