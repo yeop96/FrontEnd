e0d456f98965f4511a119b383670a90d288c993a
sujeongKim <cme10575@gmail.com>
2021-04-15 21:53:29 +0900

    파일 구조 세팅

--- App.tsx
+++ App.tsx
@@ -9,25 +9,9 @@
  */
 
 import * as React from 'react'
-import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  )
-}
-
-function MyPageScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>MyPage!</Text>
-    </View>
-  )
-}
+import { HomeScreen, MyPageScreen } from './src/pages'
 
 const Tab = createBottomTabNavigator()
 
