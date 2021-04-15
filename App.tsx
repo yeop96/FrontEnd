c680efe740c2146bb43b2e46d13f90e252b216d8
sujeongKim <cme10575@gmail.com>
2021-04-15 21:28:03 +0900

    바텀텝 추가

--- App.tsx
+++ App.tsx
@@ -11,24 +11,39 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   )
 }
 
-const Stack = createStackNavigator()
+function MyPageScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>MyPage!</Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator()
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="MyPage" component={MyPageScreen} />
+    </Tab.Navigator>
+  )
+}
 
 export function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <MyTabs />
     </NavigationContainer>
   )
 }
