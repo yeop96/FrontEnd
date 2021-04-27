6bdf188f909cd9c97e22afe924c486d2024db3ee
yeop96 <ekdh6@naver.com>
2021-04-27 17:51:48 +0900

    구글맵 라이브러리 설치

--- ios/FrontEnd/AppDelegate.m
+++ ios/FrontEnd/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -30,6 +31,8 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
+    
+    [GMSServices provideAPIKey:@"AIzaSyBQqMnnhSlGze6YvoJRTjnrs4_N6sXQljw"]; // add this line using the api key obtained from Google Console
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
