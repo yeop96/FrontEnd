72b82c2e1ed680ef102b3e173fc6976d3fcd53cd
yeop96 <ekdh6@naver.com>
2021-04-29 03:16:04 +0900

    사용자 현재 위치 ios 권한 부여 및 테스팅

---
+++ ios/geoLocation.swift
@@ -0,0 +1,8 @@
+//
+//  geoLocation.swift
+//  FrontEnd
+//
+//  Created by yeop on 2021/04/29.
+//
+
+import Foundation
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,63 @@
 import * as React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Platform, Text } from 'react-native'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { Marker } from 'react-native-maps'
+import Geolocation from 'react-native-geolocation-service'
+import { useState, useEffect } from 'react'
+
+interface ILocation {
+  latitude: number
+  longitude: number
+}
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState<ILocation | undefined>(undefined)
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords
+        setLocation({
+          latitude,
+          longitude,
+        })
+      },
+      error => {
+        console.log(error.code, error.message)
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    )
+  }, [])
+
   return (
     <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}>
-        <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title="this is a marker"
-          description="this is a marker example"
-        />
-      </MapView>
+      {location && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            //latitude: {location.longitude},
+            //longitude: location?.longitude,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}>
+          <Marker
+            coordinate={{ latitude: 37.538712, longitude: 127.082366 }}
+            title="응급실"
+            description="정신의과 전문 응급실"
+          />
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title="현재위치"
+            description="내위치"
+          />
+        </MapView>
+      )}
     </View>
   )
 }
