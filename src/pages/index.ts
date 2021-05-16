25427172152626c063d3d73c0691a17efdffc267
yeop96 <ekdh6@naver.com>
2021-05-16 19:05:40 +0900

    로그인 및 회원가입 서버연결 완료, 파일정리

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,5 @@ export { default as MyPageScreen } from './MyPage'
 export { default as ChatModal } from './ChatModal'
 export { default as SplashScreen } from './Splash'
 
-export { default as LoginScreen } from './LoginScreen'
-export { default as RegisterScreen } from './RegisterScreen'
+export { default as LoginScreen } from './Login'
+export { default as RegisterScreen } from './Register'
