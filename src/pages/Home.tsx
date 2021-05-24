0ec05d680f9c198a8a87fb6d83f82769b8df832c
sujeongKim <cme10575@gmail.com>
2021-05-24 21:28:23 +0900

    어플 안내 모달 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { Marker } from 'react-native-maps'
+import Modal from 'react-native-modal'
 import Geolocation from 'react-native-geolocation-service'
 import { useHealthInfoState } from 'context'
 import { BasicQuestionnaire } from 'pages'
+import { mainColor } from 'common'
 
 interface ILocation {
   latitude: number
   longitude: number
+}
+
+function GuideModal(props: { visible: boolean; setVisible: () => void }) {
+  return (
+    <Modal isVisible={props.visible} onBackdropPress={props.setVisible}>
+      <View style={{ backgroundColor: '#ffffff', borderRadius: 8, padding: 32 }}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 12, color: mainColor }}>안내</Text>
+        <Text style={{ fontSize: 16 }}>
+          {
+            '메디포유는 보건복지부 정책에 따라 경증 환자를 지역응급의료기관으로 안내합니다.\n대형병원의 밀집도를 낮춰 중증 환자들의 빠른 조치를 가능케 하고 경증 환자의 대기시간과 의료비용을 절감합니다.'
+          }
+        </Text>
+      </View>
+    </Modal>
+  )
 }
 
 export default function HomeScreen() {
@@ -16,6 +33,7 @@ export default function HomeScreen() {
   const [location, setLocation] = useState<ILocation | undefined>({ latitude: 37.538712, longitude: 127.082366 })
   const HealthInfo = useHealthInfoState()
   const [modalVisible, setModalVisible] = useState<boolean>(false)
+  const [infoModalVisible, setInfoModalVisible] = useState<boolean>(false)
 
   useEffect(() => {
     Geolocation.getCurrentPosition(
@@ -42,7 +60,14 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <BasicQuestionnaire modalVisible={modalVisible} setModalVisible={() => setModalVisible(false)} />
+      <BasicQuestionnaire
+        modalVisible={modalVisible}
+        setModalVisible={() => {
+          setModalVisible(false)
+          setInfoModalVisible(true)
+        }}
+      />
+      <GuideModal visible={infoModalVisible} setVisible={() => setInfoModalVisible(false)} />
       {location && (
         <MapView
           provider={PROVIDER_GOOGLE}
