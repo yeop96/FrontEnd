f20ff876a47d73dd8ff0f75f8c4a747719ca57b5
sujeongKim <cme10575@gmail.com>
2021-05-24 20:44:32 +0900

    바뀐 기획대로 진단 데이터 수정

--- src/context/HealthInfo.tsx
+++ src/context/HealthInfo.tsx
@@ -31,28 +31,10 @@ export interface BasicQuestionnaire {
   familyHistory: '암' | '뇌졸중' | '심장병(심근경색/협심증)' | '고혈압' | '당뇨병' | '이상지질혈증' | '패결핵' | '없음'
 }
 
-/** 위치 데이터 타입 */
-interface ILocation {
-  latitude: number
-  longitude: number
-}
-
-/** 병원 데이터 타입 */
-export interface Hospital {
-  name: string
-  location: ILocation
-  remainingSickbed: number
-}
-
-/** 진료과 데이터 타입*/
-export interface Department {
-  name: string
-  hospital: Hospital[]
-}
-
 export interface Disease {
   name: string
   description: string
+  level: '경증 질환' | '중증 질환'
 }
 
 /** 초진 데이터 타입 */
@@ -60,7 +42,9 @@ export interface Diagnosis {
   date: Date
   symptom: string[]
   disease: Disease[]
-  department: Department[]
+  /** 병원 정보 협의 후에 수정 */
+  hospital: any
+  medicationBeingTaken: string
 }
 
 export interface HealthInfo {
@@ -76,84 +60,60 @@ const initialState: HealthInfo = {
       symptom: ['배 아픔', '열이 남', '설사'],
       disease: [
         {
-          name: '위장염',
+          name: '위장염1',
           description:
             '급성 위장염은 위장관의 염증 상태를 말하여 위나 소장에 염증이 발생한 것을 말합니다. 바이러스의 감염, 박테리아나 기생충의 감염에 의해 발생하게 됩니다.',
+          level: '경증 질환',
         },
-        { name: '식중독', description: '식중독은 상한 음식을 먹었을 때 주로 발생하며' },
+        { name: '식중독', description: '식중독은 상한 음식을 먹었을 때 주로 발생하며', level: '경증 질환' },
         {
           name: '장폐색',
           description: '장폐색은 블라블라 때문에 발생하며 뭐뭐를 증상으로 가진다. 즉시 조치해야 한다.',
+          level: '중증 질환',
         },
       ],
-      department: [
-        {
-          name: '내과',
-          hospital: [{ name: '홍익병원', location: { latitude: 233, longitude: 155 }, remainingSickbed: 3 }],
-        },
-        {
-          name: '가정의학과',
-          hospital: [{ name: '세브란스병원', location: { latitude: 200, longitude: 151 }, remainingSickbed: 13 }],
-        },
-      ],
+      hospital: 'something',
+      medicationBeingTaken: '없음',
     },
     {
       date: new Date('2020-12-17T03:24:00'),
       symptom: ['기침', '열이 남', '토함', '기침', '열이 남', '토함'],
       disease: [
         {
-          name: '위장염',
+          name: '위장염2',
           description:
             '급성 위장염은 위장관의 염증 상태를 말하여 위나 소장에 염증이 발생한 것을 말합니다. 바이러스의 감염, 박테리아나 기생충의 감염에 의해 발생하게 됩니다.',
+          level: '경증 질환',
         },
-        { name: '식중독', description: '식중독은 상한 음식을 먹었을 때 주로 발생하며' },
+        { name: '식중독', description: '식중독은 상한 음식을 먹었을 때 주로 발생하며', level: '경증 질환' },
         {
           name: '장폐색',
           description: '장폐색은 블라블라 때문에 발생하며 뭐뭐를 증상으로 가진다. 즉시 조치해야 한다.',
+          level: '경증 질환',
         },
       ],
-      department: [
-        {
-          name: '내과',
-          hospital: [{ name: '홍익병원', location: { latitude: 233, longitude: 155 }, remainingSickbed: 3 }],
-        },
-        {
-          name: '가정의학과',
-          hospital: [{ name: '세브란스병원', location: { latitude: 200, longitude: 151 }, remainingSickbed: 13 }],
-        },
-      ],
+      hospital: 'sample',
+      medicationBeingTaken: '감기약',
     },
     {
       date: new Date('2020-12-17T03:24:00'),
       symptom: ['배 아픔', '열이 남', '설사'],
       disease: [
         {
-          name: '위장염',
+          name: '위장염3',
           description:
             '급성 위장염은 위장관의 염증 상태를 말하여 위나 소장에 염증이 발생한 것을 말합니다. 바이러스의 감염, 박테리아나 기생충의 감염에 의해 발생하게 됩니다.',
+          level: '경증 질환',
         },
-        { name: '식중독', description: '식중독은 상한 음식을 먹었을 때 주로 발생하며' },
+        { name: '식중독', description: '식중독은 상한 음식을 먹었을 때 주로 발생하며', level: '경증 질환' },
         {
           name: '장폐색',
           description: '장폐색은 블라블라 때문에 발생하며 뭐뭐를 증상으로 가진다. 즉시 조치해야 한다.',
+          level: '경증 질환',
         },
       ],
-      department: [
-        {
-          name: '이비인후과',
-          hospital: [
-            { name: '양천병원', location: { latitude: 233, longitude: 155 }, remainingSickbed: 3 },
-            { name: '양천병원', location: { latitude: 244, longitude: 155 }, remainingSickbed: 3 },
-          ],
-        },
-        {
-          name: '기관지외과',
-          hospital: [
-            { name: '광진병원', location: { latitude: 200, longitude: 151 }, remainingSickbed: 13 },
-            { name: '양천병원', location: { latitude: 233, longitude: 155 }, remainingSickbed: 3 },
-          ],
-        },
-      ],
+      hospital: 'temp',
+      medicationBeingTaken: '없음',
     },
   ],
 }
