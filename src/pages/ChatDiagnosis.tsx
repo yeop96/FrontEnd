e477df47a54f13bac887533c602a5cc5cd77330e
yeop96 <ekdh6@naver.com>
2021-05-27 02:48:26 +0900

    검진 결과 화면 map, 채팅 모달에서 검진결과 띄어주기 구현

--- src/pages/ChatDiagnosis.tsx
+++ src/pages/ChatDiagnosis.tsx
@@ -1,15 +1,45 @@
-import * as React from 'react'
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native'
 import { format } from 'date-fns'
 import { mainColor } from 'common'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { Marker } from 'react-native-maps'
+import Geolocation from 'react-native-geolocation-service'
 
 const questionnairekey = ['흡연', '음주', '운동', '병력', '가족력']
+const { height, width } = Dimensions.get('window')
+
+interface ILocation {
+  latitude: number
+  longitude: number
+}
 
 /** 가짜 기초문진내역, 필요시 인덱스별로 가져다 쓰도록 함 */
 const tempBasicQuestionnaire = ['피우지 않습니다', '일주일에 두 번', '일주일에 한 번', '당뇨병', '암']
 
 /** 초진내역 */
 export default function ChatDiagnosis(props: { diagnosis: any }) {
+  const [location, setLocation] = useState<ILocation | undefined>({ latitude: 37.538712, longitude: 127.082366 })
+
+  //위치 구하기
+  useEffect(() => {
+    // 현재 위치 담아두기
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLocation({
+          latitude,
+          longitude,
+        })
+      },
+      (error) => {
+        console.log(error.code, error.message)
+      },
+      // 타임아웃 거는거
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    )
+  }, [])
+
   let isAllLightDiease = true
   props.diagnosis.disease.map((diseaseInfo) => {
     if (diseaseInfo.level !== '경증 질환') {
@@ -70,7 +100,33 @@ export default function ChatDiagnosis(props: { diagnosis: any }) {
         </View>
       </View>
 
-      {/* 병원 지도 넣을 자리*/}
+      {/* 병원 지도 */}
+      <Text style={[style.textSemiTitle, { marginTop: 18 }]}>병원 안내</Text>
+      <View style={{ alignItems: 'center' }}>
+        {location && (
+          <MapView
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            provider={PROVIDER_GOOGLE}
+            style={style.map}
+            region={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+              latitudeDelta: 0.0522,
+              longitudeDelta: 0.0021,
+            }}>
+            <Marker
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+              title="현재위치"
+              description="내위치"
+            />
+          </MapView>
+        )}
+      </View>
+
       {/** 바텀텝에 가려지지 않도록 띄워주는 빈 공간 */}
       <View style={{ height: 60 }} />
     </ScrollView>
@@ -104,5 +160,10 @@ const style = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
     fontSize: 24,
+  },
+  map: {
+    height: width - 32,
+    width: width - 32,
+    borderRadius: 10,
   },
 })
